382de2fbcce028d7fec9b4bed7efc0a4b9d957d4
OscarSilvaOfficial <oscarkaka222@gmail.com>
2024-09-18 19:55:40 -0300

    chore: added form

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
+  if request.method == 'POST':
+    description = request.POST.get('description')
+    return HttpResponse(f"Description received: {description}")
+  
   return render(request, 'index.html') 
